3fd01c6f71472dcee4055cb0cec62a311e1c0c4b
devAYZ <fatokimiemman@gmail.com>
2024-08-11 14:30:19 +0100

    coordinate app flow

---
+++ tableView-prep/IntroViewController.swift
@@ -0,0 +1,40 @@
+//
+//  IntroViewController.swift
+//  tableView-prep
+//
+//  Created by Ayokunle Pro on 8/11/24.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .systemTeal
+    }
+
+
+}
+
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .systemTeal
+    }
+
+
+}
+
