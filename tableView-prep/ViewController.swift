29bc70ad42e0c922fec902d8ff6806acdb3f1b88
devAYZ <fatokimiemman@gmail.com>
2024-08-11 14:16:15 +0100

    Initial Commit

---
+++ tableView-prep/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableView-prep
+//
+//  Created by Ayokunle Pro on 8/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
